7bd72b82f0f36a9e7e717aadca12cf2a6fc2b283
ilyassdaaif <94968783+ilyassdaaif@users.noreply.github.com>
2024-05-13 02:21:48 +0100

    Update app.py

--- 0x01-challenge/status_server/api/v1/app.py
+++ 0x01-challenge/status_server/api/v1/app.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
-Web server
+Web server 
 """
 from api.v1.views import app_views
 from flask import Flask, jsonify, make_response
 
-app = Falsk(__name__)
+app = Flask(__name__)
 app.register_blueprint(app_views)
 
 
@@ -16,5 +16,5 @@ def not_found(error):
 
 
 if __name__ == "__main__":
-    # python -m api.v1.app
+    # python -m api.v1.app 
     app.run(host="0.0.0.0", port=5000)
